fe34e3aa4763564366b70f298367f11ddff29e6d
yeasin507 <101644122+yeasin507@users.noreply.github.com>
2022-06-11 22:58:23 +0600

    Add files via upload

--- java/index.js
+++ java/index.js
@@ -1,134 +1,29 @@
+var hamburger = document.querySelector(".hamburger");
+var bar = document.querySelector(".bar");
+var navLink = document.querySelector(".nav_link");
 
+hamburger.addEventListener("click", ()=>{
+    hamburger.classList.toggle("active");
+  navLink.classList.toggle("height_auto");
+});
 
 
-// document.getElementById("ton").addEventListener("click",run)
+const faqs = document.querySelectorAll(".faq"); 
 
-// function run(){
+faqs.forEach( (faq) => {
+  faq.addEventListener("click",() =>{
+  faq.classList.toggle("achsd");
+  })
+});
 
-// var nextIndex = document.getElementById("press").value;
-// var password= "i";// am yeasin arafat open it
 
-// if(nextIndex == password){
-//     result()
-// }else{
-//     // alert("You don't have permission to enter here")
-//     s()
-//     document.getElementById("sr").innerHTML= "You don't have permission to enter here";
-// }
-// }
 
-// function result(){
-//     document.querySelector(".font_input").style.display="none"
-//     document.querySelector(".way_text").style.display="none"
-//     document.querySelector(".next").style.display="block"
-//     document.querySelector(".my_name").style.display="block"
-//     document.querySelector("#ton").style.display="none"
-//     document.querySelector("#tun").style.display="block"
-// }
-// function s(){
-//     document.querySelector(".font_input").style.display="none"
-//     document.querySelector(".s_rc").style.display="block"
-// }
+// (function($){
+// 	$(function(){
 
 
-// =====================================================================================================================
-// =====================================================================================================================
-// =====================================================================================================================
 
-
-
-
-//    I finally finished the work at 2 o'clock at night 🥳🥳🥳
-
-
-function mf(event) {
-  var x = event.key;
-  if (x == "Enter") { 
-    var nextIndex = document.getElementById("press").value;
-    var password= "i am yeasin arafat open it";
-    
-    if(nextIndex == password){
-        result()
-    }else{
-        s()
-        document.getElementById("sr").innerHTML= "You don't have permission to enter here";
-    }
-  }
-}
-
-
-function result(){
-    document.querySelector(".font_input").style.display="none"
-    document.querySelector(".way_text").style.display="none"
-    document.querySelector(".next").style.display="block"
-    document.querySelector(".my_name").style.display="block"
-    document.querySelector(".open").style.display="block"
-
-}
-function s(){
-    document.querySelector(".font_input").style.display="none"
-    document.querySelector(".s_rc").style.display="block"
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//         // Phone nav click function
+//         $('.hamburger').click(function () {
+//             $("body").toggleClass("navShown");
+//             $(".nav-wrap").;
